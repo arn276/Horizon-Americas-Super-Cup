02e43d4eb9752d85849ef0fc86ecfbc4a2fa1ed2
Aaron Wilson <aaron.wilson.0740@gmail.com>
2024-12-24 12:11:16 -0600

    Update matchuping

--- windup/CreateSchedule.py
+++ windup/CreateSchedule.py
@@ -6,7 +6,7 @@ Created on Sat Dec 21 21:23:50 2024
 """
 
 import pandas as pd
-import random
+import random, itertools
 from League_Info import leagueFormation
 
 leageDict = leagueFormation.leagueDict()
@@ -21,37 +21,41 @@ def selectGrpMatchups(groupTeams,matchups_grpRd):
     matchup2 = random.choice([x for x in matchups_grpRd if t1 not in x and t2[0] not in x])
     return [matchup1,matchup2]
 
-
+def flattenLsts(lst):
+    return list(itertools.chain.from_iterable(lst))
 
 
 ## Convert League Dictionary to lists
-buildConf = []
+conferenceTms,divisionTms, groupTms = [],[],[]
 for conf in leageDict.keys(): 
     confLst = []
     for div in leageDict[conf].keys():
         divLst = []
+        
         for grp in leageDict[conf][div].keys():
-            divLst.append(list(leageDict[conf][div][grp].keys()))
-        confLst.append(divLst)
-    buildConf.append(confLst)
-
-
-matchups = []
-confLst = []
-for conf in buildConf:
-    # Group pairings
-    divLst = []
-    for div in conf:
-        grpLst = []
-        for grp in div:
-            teamLst =[]
-            for t in grp:
-                for o in grp:
-                    if t!=o:
-                        teamLst.append([t, o])
-            grpLst.append(teamLst)
-        divLst.append(grpLst)
-    confLst.append(divLst)
-    # matchups_grpplay.append(grpLst)
+            groupLst = list(leageDict[conf][div][grp].keys())
+            groupTms.append(groupLst)
+            divLst.append(groupLst)
+        divisionTms.append(flattenLsts(divLst))
+        confLst.append(flattenLsts(divLst))
+    conferenceTms.append(flattenLsts(confLst))
     
 
+
+
+def categorizeMatchup():
+    
+
+
+## Find all possible matchups - Home and Away
+confMatchups = []
+for conf in conferenceTms:
+    matchupLst = []
+    for h in conf:
+        for a in conf:
+            if h != a:
+                
+                matchupLst.append([h,a])
+    confMatchups.append(matchupLst)
+
+conferenceTms[0]
