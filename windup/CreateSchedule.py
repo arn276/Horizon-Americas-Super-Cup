ba568a5b200f79bf1d3107d779ffe29576934abd
Aaron Wilson <aaron.wilson.0740@gmail.com>
2025-01-12 11:14:53 -0600

    Simulate w-l

--- windup/CreateSchedule.py
+++ windup/CreateSchedule.py
@@ -23,47 +23,47 @@ leagueFormat,conferenceTms,divisionTms,groupTms = leagueFormation.teamLsts(leage
 ## Find all possible matchups - Home and Away
 confMatchups = matchups.allPosibleMatchups(conferenceTms,divisionTms,groupTms)
 
-#### Setup Conference Pairings
-## Finding Unique group pairings
-uniqueConfPairingOptions = matchups.allUniquePairs(leagueFormat)
+# #### Setup Conference Pairings
+# ## Finding Unique group pairings
+# uniqueConfPairingOptions = matchups.allUniquePairs(leagueFormat)
 
-## Group Scheduling
-maxGroupGames = len(confMatchups[0][0][1:])*confMatchups[0][0][2][3]
-groupMatchups = matchups.cycleGroups(confMatchups, 0, maxGroupGames, 18)
+# ## Group Scheduling
+# maxGroupGames = len(confMatchups[0][0][1:])*confMatchups[0][0][2][3]
+# groupMatchups = matchups.cycleGroups(confMatchups, 0, maxGroupGames, 18)
 
-## Division Scheduling
-maxDivisionGames = len(confMatchups[0][1][1:])*confMatchups[0][1][2][3]
-divisionMatchups = matchups.cycleGroups(confMatchups, 1, maxDivisionGames, 16)
+# ## Division Scheduling
+# maxDivisionGames = len(confMatchups[0][1][1:])*confMatchups[0][1][2][3]
+# divisionMatchups = matchups.cycleGroups(confMatchups, 1, maxDivisionGames, 16)
 
-## Conference Scheduling   
-conferenceMatchups,conferenceMatchups2 = matchups.conferenceScheduling(uniqueConfPairingOptions, confMatchups)
+# ## Conference Scheduling   
+# conferenceMatchups,conferenceMatchups2 = matchups.conferenceScheduling(uniqueConfPairingOptions, confMatchups)
          
-#### Combine all the matchups to a single list
-AllMatchups = groupMatchups+divisionMatchups+conferenceMatchups+conferenceMatchups2
+# #### Combine all the matchups to a single list
+# AllMatchups = groupMatchups+divisionMatchups+conferenceMatchups+conferenceMatchups2
 
-######################
-#### Create Schedule
-conf1,conf2 = schedule.createOrderOfGames(AllMatchups)
+# ######################
+# #### Create Schedule
+# conf1,conf2 = schedule.createOrderOfGames(AllMatchups)
 
-## Add Dates
-base = datetime.date(2010, 4, 1)
+# ## Add Dates
+# base = datetime.date(2010, 4, 1)
 
-schedule_conf1 = schedule.setDates(conf1,base)
-schedule_conf2 = schedule.setDates(conf2,base)
-
-
-#### Find series to make 4 games
-schedule_conf1 = schedule.groupSeriesToMake4Games(base,schedule_conf1,groupTms)
-schedule_conf2 = schedule.groupSeriesToMake4Games(base,schedule_conf2,groupTms)
+# schedule_conf1 = schedule.setDates(conf1,base)
+# schedule_conf2 = schedule.setDates(conf2,base)
 
 
-with open(r'C:\Users\aaron\OneDrive\Documents\GitHub\FoundersSchedule.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(schedule_conf1)
+# #### Find series to make 4 games
+# schedule_conf1 = schedule.groupSeriesToMake4Games(base,schedule_conf1,groupTms)
+# schedule_conf2 = schedule.groupSeriesToMake4Games(base,schedule_conf2,groupTms)
 
-with open(r'C:\Users\aaron\OneDrive\Documents\GitHub\VisionariesSchedule.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(schedule_conf2)
+
+# with open(r'C:\Users\aaron\OneDrive\Documents\GitHub\FoundersSchedule.csv', 'w', newline='') as f:
+#     writer = csv.writer(f)
+#     writer.writerows(schedule_conf1)
+
+# with open(r'C:\Users\aaron\OneDrive\Documents\GitHub\VisionariesSchedule.csv', 'w', newline='') as f:
+#     writer = csv.writer(f)
+#     writer.writerows(schedule_conf2)
 
 
 
@@ -73,6 +73,7 @@ with open(r'C:\Users\aaron\OneDrive\Documents\GitHub\VisionariesSchedule.csv', '
 locationStr = r'C:\Users\Public\retrosheets\gl2020_23'
 season24 = historicSeasons.readSeason(locationStr+r'\gl2024.txt')
 
+# Score commonality
 regulation = season24[(season24['lengthofgame_outs']>=51) | (season24['lengthofgame_outs']<=54) ]
 resultRate = regulation[['roadscor','homescore']].value_counts().reset_index()
 resultRate['PercentOfTotal'] = resultRate['count']/resultRate['count'].sum()
@@ -80,7 +81,9 @@ resultRate['PercentOfTotal'] = resultRate['count']/resultRate['count'].sum()
 resultRate.loc[resultRate['homescore'] > resultRate['roadscor'], 'winner'] = 'Home'
 resultRate.loc[resultRate['homescore'] < resultRate['roadscor'], 'winner'] = 'Road'
 
-
+# rate of going to extras
+extras = season24[season24['lengthofgame_outs']>54]
+extrasRate = round(len(extras)/len(season24),3)*100
 
 ##############################
 ####Historic Standings
@@ -126,6 +129,10 @@ for lg in lgIds:
     rankAvgWinPct.append(league)
 
 
+#### "Play" the games
+##########################
+
+
 # Simulate team strength
 conf = copy.deepcopy(conferenceTms)
 teamStength = []
@@ -135,22 +142,58 @@ for i in range(len(rankAvgWinPct)):
         conf[i].remove(team)
         teamStength.append([team,stength])
 
+# Open conference schedules
+with open(r'C:\Users\aaron\OneDrive\Documents\GitHub\FoundersSchedule.csv', 'r') as f:
+    reader = csv.reader(f)
+    schedule_conf1 = list(reader)
+
+# Open conference schedules
+with open(r'C:\Users\aaron\OneDrive\Documents\GitHub\VisionariesSchedule.csv', 'r') as f:
+    reader = csv.reader(f)
+    schedule_conf2 = list(reader)
+
+#Simulate Win-Loss
+results_conf1=[]
+for game in schedule_conf1:
+    if game[1] == '':
+        results_conf1.append(game+[''])
+    else:
+        hometeam = game[1]
+        awayteam = game[2]
+        
+        # Find home-away weight 
+        homeWt = [weight[1]*100 for weight in teamStength if weight[0] == hometeam][0]+3
+        awayWt = [weight[1]*100 for weight in teamStength if weight[0] == awayteam][0]-3
+        
+        # Balance to 100% odds
+        totalStrgthWt = homeWt+awayWt
+        adj = 100/totalStrgthWt
+        homeWt,awayWt = homeWt*adj,awayWt*adj
+        
+        #3 % change for for home field advantage
+        homeWt,awayWt = homeWt+3,awayWt-3
+        
+        # Change for 
+        homeWt,awayWt = homeWt-(extrasRate/2),awayWt-(extrasRate/2)
+        
+        # Result weight based selection
+        randomList = random.choices(game[1:]+['tie'], weights=(homeWt, awayWt,extrasRate), k=1)
+        commonWinnerSelection = max(set(randomList), key=randomList.count)
+        results_conf1.append(game+[commonWinnerSelection])
+
+
+
+resultRate[:20]
+
+
+with open(r'C:\Users\aaron\OneDrive\Documents\GitHub\FoundersResultes.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(results_conf1)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+tie = random.choices([False,True], weights=(100-extrasRate,extrasRate), k=1)
 
 
 
